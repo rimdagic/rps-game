c774017aaee0f1ee1bb239f3caeacf72fc4532a0
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-10-03 16:36:58 +0200

    edited namnis

--- src/main/java/org/example/Opponents/Namnis.java
+++ src/main/java/org/example/Opponents/Namnis.java
@@ -2,8 +2,6 @@ package org.example.Opponents;
 
 import org.example.Move;
 import org.example.Player;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -20,21 +18,36 @@ public class Namnis implements BehaviourInterface {
         Random random = new Random();
         int index = random.nextInt(Move.values().length);
 
-        List<String> rockVowels = List.of(
+        List<String> someVowels = List.of(
                 "A", "a",
                 "U", "u",
                 "I", "i"
         );
 
-        for ( String letter : rockVowels ) {
-            if (player.getName().contains(letter)) {
-                index = 0;
-                break;
-            }
+        List<String> otherVowels = List.of(
+                "O", "o",
+                "E", "e"
+        );
+
+        if(containsLetterFromList(someVowels) && index == 0){
+            index = 2;
+        } else if (containsLetterFromList(otherVowels) && index == 1) {
+            index = 0;
+        } else if (!containsLetterFromList(someVowels) && !containsLetterFromList(otherVowels) && index == 2) {
+            index = 1;
         }
 
         return Move.values()[index];
     }
 
+    private boolean containsLetterFromList(List<String> letters){
+        boolean isContaining = false;
+        for ( String letter : letters ) {
+            if (player.getName().contains(letter)) {
+                isContaining = true;
+            }
+        }
+        return isContaining;
+    }
 
 }
